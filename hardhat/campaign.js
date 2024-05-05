036d24ef57866a97674e210f4f9b41447726ae47
steph-s2bc-dev <stephane.vrignaud@s2bconnected.com>
2024-05-05 15:48:41 +0700

    reset

--- hardhat/campaign.js
+++
@@ -1,7 +0,0 @@
-import { ethers } from 'hardhat';
-import Campaign from './artifacts/contracts/CrowdCollab.sol/CrowdCollab.json';
-
-export default function getContractInstance(address) {
-  const provider = ethers.provider; // This line is fine if you are using the global provider
-  return new ethers.Contract(address, Campaign.abi, provider);
-}
--- hardhat/factory.js
+++
@@ -1,9 +0,0 @@
-import { ethers } from 'hardhat';
-import CampaignFactory from './artifacts/contracts/CampaignCreator.sol/CampaignCreator.json';
-
-const campaignFactoryAddress = "0x3983204c96C909c4796Ad933746BfFee767C768c";
-
-export async function getCampaignFactoryInstance() {
-  const provider = ethers.provider; // This line is fine if you are using the global provider
-  return new ethers.Contract(campaignFactoryAddress, CampaignFactory.abi, provider);
-}
