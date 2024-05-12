4fcb517f3559265a56b9c377d63de269dba0bcd8
steph-s2bc-dev <stephane.vrignaud@s2bconnected.com>
2024-05-12 22:46:02 +0700

    contribute wallet2

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -1,6 +1,5 @@
 import styles from "./page.module.css";
 import InteractContract from "./interact-contract";
-import CampaignInteraction from "./CampaignInteraction";
 
 export default function Home() {
   return (
